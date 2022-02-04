73d785e29a54ee399ed3dd3f4a987a4b702ab95f
Daiyaan Arfeen <marfeen@andrew.cmu.edu>
2022-02-04 20:20:44 +0000

    large dlrm flags

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=7 python -m pdb main.py --mini-batch-size=2 --data-size=6 --profile --use-gpu --verbose
+CUDA_VISIBLE_DEVICES=7 python -m pdb main.py --arch-sparse-feature-size=64 --arch-mlp-bot=512-512-64 --arch-mlp-top=1024-1024-1024-1 \
+  --num-indices-per-lookup=100 --mini-batch-size=2 --data-size=6 --profile --use-gpu --verbose
